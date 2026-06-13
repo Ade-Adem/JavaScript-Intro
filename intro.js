eac89ab844f562df0641a7cb0372ceccf4cb80bc
Ade-Adem <adekolaademola347@gmail.com>
2026-06-13 15:34:58 +0100

    Intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "ADEKOLA, Ademola";
+const age = 22;
+const FPL = "Javascript";
+
+console.log("NAME:", name);
+console.log("AGE:", age);
+console.log("Favorite Programming Language:", FPL);
+
